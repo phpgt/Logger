11bf1d03e98d4c008e47ab0b955f7bd6e766a78a
Greg Bowler <greg.bowler@g105b.com>
2021-06-16 23:24:48 +0100

    feature: StdOutHandler can be formatted

--- src/LogHandler/StdOutHandler.php
+++ src/LogHandler/StdOutHandler.php
@@ -2,7 +2,18 @@
 namespace Gt\Logger\LogHandler;
 
 class StdOutHandler extends FileHandler {
-	public function __construct() {
-		parent::__construct("php://stdout");
+	public function __construct(
+		string $timestampFormat = "Y-m-d H:i:s",
+		array $logFormat = self::DEFAULT_LOG_FORMAT,
+		string $separator = self::DEFAULT_SEPARATOR,
+		string $newLine = self::DEFAULT_LOG_LINE_ENDING
+	) {
+		parent::__construct(
+			"php://stdout",
+			$timestampFormat,
+			$logFormat,
+			$separator,
+			$newLine
+		);
 	}
 }
